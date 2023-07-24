5a4dfbfd2c7028d689d4b283827150bde040380d
Charlie Marshak <charlie.z.marshak@jpl.nasa.gov>
2023-07-24 15:08:17 -0700

    fix build

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,6 +15,7 @@ def orbit_dict_for_azimuth_test():
     return {'reference': test_data / 'S1B_OPER_AUX_POEORB_OPOD_20210812T111941_V20210722T225942_20210724T005942.EOF',
             'secondary': test_data / 'S1B_OPER_AUX_POEORB_OPOD_20210731T111940_V20210710T225942_20210712T005942.EOF'}
 
+
 @pytest.fixture(scope='session')
 def slc_id_dict_for_azimuth_test():
     return {'reference': test_data / 'S1B_IW_SLC__1SDV_20210723T014947_20210723T015014_027915_0354B4_B3A9',
